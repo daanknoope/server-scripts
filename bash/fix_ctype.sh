4561c507a45f2318565bb62f91fd33f3fea4c226
Daan Knoope <daan@knoope.dev>
2019-12-15 22:49:19 +0100

    Add attribution

--- bash/fix_ctype.sh
+++ bash/fix_ctype.sh
@@ -1,3 +1,4 @@
 # Appends export to bashrc which fixes autocomplete issues in bash for new users on RHEL 8.1
+# See: https://storagenerd.wordpress.com/2018/09/13/how-to-fix-warning-setlocale-lc_ctype-cannot-change-locale-utf-8/
 
 echo "export LC_CTYPE=\$LANG" >> ~/.bashrc
