0b6fff87900ea8129a76fc58e32e4a67561ef4eb
Daan Knoope <daan@knoope.dev>
2019-12-15 22:48:44 +0100

    Add script for installing EPEL repos in RHEL 8

---
+++ RHEL/add_EPEL.sh
@@ -0,0 +1,4 @@
+# Adds EPEL repository to RHEL 8.1 so additional programs like htop can be installed
+
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+sudo yum update
